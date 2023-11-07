407eac576cc80041e48ebc026c27f9db31a6b24f
koba1217 <kobayashi.yhm@om.asahi-kasei.co.jp>
2023-11-07 16:58:22 +0900

    ログイン画面の修正

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,32 +1,32 @@
 <template>
-    <v-app>
-      <v-card class="mx-auto">
-        <v-card-title>
-            <p>{{title}}</p>
-        </v-card-title>
-        <v-card-text>
-            <v-form>
-                <v-text-field prepend-icon="mdi-account-circle" label="メールアドレス" />
-                <v-text-field v-bind:type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword" prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
-                <v-card-actions>
-                    <v-btn class="info" variant="outlined">ログイン</v-btn>
-                </v-card-actions>
-            </v-form>
-        </v-card-text>
-      </v-card>
-    </v-app>
+  <v-card class="mx-auto my-auto">
+    <v-card-text>
+      <p class="caption mb-4">{{title}}</p>
+      <v-form>
+        <v-text-field prepend-icon="mdi-account-circle" v-model="name" label="メールアドレス" />
+        <v-text-field v-bind:type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword" prepend-icon="mdi-lock" append-icon="mdi-eye-off" v-model="password" label="パスワード" />
+        <v-card-actions>
+          <v-btn class="mx-auto my-auto" color="blue-darken-3" variant="flat" @click="submit">ログイン</v-btn>
+          <v-btn class="mx-auto my-auto" color="blue-darken-3" variant="outlined">クリア</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
- <script>
+<script>
  export default {
    name: "App",
- 
+
    data: () => ({
     title: "メールアドレス、パスワードを入力してください。",
     showPassword: false,
+    name:'',
+    password:'',
     home: {
       icon: "mdi-home",
     },
    }),
+   methods:{submit(){alert(this.name+","+this.password) } },
  };
- </script>
+</script>
